d8f1d6857fcf02f3d2d82327a13ae7eba5d332d3
entaku <entaku19890818@gmail.com>
2024-01-18 10:47:55 +0900

    add

---
+++ AudioMasterApp/AudioMasterApp/AudioMasterApp/AudioMasterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioMasterAppApp.swift
+//  AudioMasterApp
+//
+//  Created by 遠藤拓弥 on 2024/01/18.
+//
+
+import SwiftUI
+
+@main
+struct AudioMasterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioMasterApp/AudioMasterApp/AudioMasterApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioMasterApp
+//
+//  Created by 遠藤拓弥 on 2024/01/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
